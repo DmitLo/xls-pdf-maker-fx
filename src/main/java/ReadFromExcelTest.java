472bd33b8eaf1bc79a56c9fe97d136012a05cffe
Dzmitry Los <d_los@bk.ru>
2025-10-10 00:29:11 +0300

    add count number row and collumn

--- src/main/java/ReadFromExcelTest.java
+++ src/main/java/ReadFromExcelTest.java
@@ -3,6 +3,7 @@ import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.ClientAnchor;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFClientAnchor;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -43,7 +44,7 @@ public class ReadFromExcelTest {
         //   materialsEquipmentString = "Материалы, изделия, конструкции";
         //}
 
-        //Read read = new Read(new ArrayList<>(0));
+//        Read read = new Read(new ArrayList<>(0));
 //        List<String> read1 = new ArrayList<>(0);
 //        List<String> read2 = new ArrayList<>(0);
 //        List<String> read3 = new ArrayList<>(0);
@@ -144,10 +145,69 @@ public class ReadFromExcelTest {
             // ... ваш код
             //}
 
-            //копирование и создание новой области
-            HSSFSheet sourceSheet = sourceWb.getSheet("Лист1"); // Получаем исходный лист
-            HSSFSheet newSheet = sourceWb.cloneSheet(sourceWb.getSheetIndex(sourceSheet));
-            newSheet.getSheetName(); // Устанавливаем новое имя для скопированного листа
+            //количество листов в документе
+            int numberOfSheets = sourceWb.getNumberOfSheets();
+
+            for (int i = 0; i < numberOfSheets; i++) {
+                HSSFSheet sourceSheet = sourceWb.getSheet("Лист" + (i + 1)); // Получаем исходный лист
+
+
+                //количество заполненных строк и столбцов в документе
+                Sheet sheet = sourceWb.getSheetAt(i); // Получаем первый лист
+
+                // Подсчет строк
+                int lastRowNum = sheet.getLastRowNum(); // Получаем индекс последней строки (с данными)
+                int totalRowsWithData = lastRowNum + 1; // Общее количество строк с данными
+
+                // Подсчет столбцов
+                // Чтобы найти последний заполненный столбец, нужно найти максимальное значение getLastCellNum() среди всех строк.
+                short maxColNum = 0;
+                for (int j = 0; j <= lastRowNum; j++) {
+                    if (sheet.getRow(j) != null) {
+                        if (sheet.getRow(j).getLastCellNum() > maxColNum) {
+                            maxColNum = sheet.getRow(j).getLastCellNum();
+                        }
+                    }
+                }
+                int totalColumnsWithData = maxColNum; // Количество столбцов с данными
+
+                //количество строк и столбцов в документе
+                System.out.println(totalRowsWithData + " " + totalColumnsWithData);
+
+                ///
+
+                List<String> read1 = new ArrayList<>(0);
+                List<String> read2 = new ArrayList<>(0);
+                List<String> read3 = new ArrayList<>(0);
+                List<String> read4 = new ArrayList<>(0);
+                List<String> read5 = new ArrayList<>(0);
+                List<String> read6 = new ArrayList<>(0);
+                List<String> read7 = new ArrayList<>(0);
+
+                List<Read> read = new ArrayList<>();
+                Read read11 = new Read(read1);
+                Read read22 = new Read(read2);
+                Read read33 = new Read(read3);
+                Read read44 = new Read(read4);
+                Read read55 = new Read(read5);
+                Read read66 = new Read(read6);
+                Read read77 = new Read(read7);
+
+                read.add(read11);
+                read.add(read22);
+                read.add(read33);
+                read.add(read44);
+                read.add(read55);
+                read.add(read66);
+                read.add(read77);
+
+                ///
+
+
+                //копирование и создание новой области
+                //HSSFSheet sourceSheet = sourceWb.getSheet("Лист1"); // Получаем исходный лист
+                //HSSFSheet newSheet = sourceWb.cloneSheet(sourceWb.getSheetIndex(sourceSheet));
+                // newSheet.getSheetName(); // Устанавливаем новое имя для скопированного листа
 
 //            //сохранение изменений
 //            try (FileOutputStream fos = new FileOutputStream("object.xls")) {
@@ -155,38 +215,41 @@ public class ReadFromExcelTest {
 //            }
 //        }
 
-            //----------
+                //----------
 
 
 //        //создание книги и листа
 //        XSSFWorkbook workbook = new XSSFWorkbook();
 //        XSSFSheet sheet = workbook.createSheet("Лист с рисунком");
 
-            //загрузка изображения м создание битового массива
-            String imagePath = "П13.png";
-            FileInputStream inputStream = new FileInputStream(imagePath);
-            byte[] imageBytes = inputStream.readAllBytes();
-            inputStream.close();
+                //загрузка изображения м создание битового массива
+                String imagePath = "П13.png";
+                FileInputStream inputStream = new FileInputStream(imagePath);
+                byte[] imageBytes = inputStream.readAllBytes();
+                inputStream.close();
 
-            //добавление рисунка на лист
+                //добавление рисунка на лист
 //        int pictureIndex = workbook.addPicture(imageBytes, XSSFWorkbook.PICTURE_TYPE_PNG);
 //        ClientAnchor anchor = new XSSFClientAnchor(0, 0, 0, 0, 0, 0, 2, 2);
 //        anchor.setAnchorType(ClientAnchor.AnchorType.MOVE_AND_RESIZE); //изменение размера при изменении ячейки
 //        sheet.createDrawingPatriarch().createPicture(anchor, pictureIndex);
 
-            int pictureIndex = sourceWb.addPicture(imageBytes, XSSFWorkbook.PICTURE_TYPE_PNG);
-            ClientAnchor anchor = new HSSFClientAnchor(0, 0, 0, 0, (short) 1, 16, (short) 3, 18);
-            //ClientAnchor anchor = new HSSFClientAnchor(0, 0, 0, 0, (short) 0, 0, (short) 18, 2);
-            //ClientAnchor anchor = new HSSFClientAnchor(0, 0, 0, 0, (short) 0, 0, (short) 18, 2);
-            //ClientAnchor anchor = new HSSFClientAnchor(0, 0, 0, 0, (short) 0, 0, (short) 18, 2);
-            anchor.setAnchorType(ClientAnchor.AnchorType.MOVE_AND_RESIZE); //изменение размера при изменении ячейки
-            newSheet.createDrawingPatriarch().createPicture(anchor, pictureIndex);
+                int pictureIndex = sourceWb.addPicture(imageBytes, XSSFWorkbook.PICTURE_TYPE_PNG);
+                ClientAnchor anchor = new HSSFClientAnchor(1020, 0, 1023, 0, (short) 2, 16, (short) 3, 18);
+                //ClientAnchor anchor = new HSSFClientAnchor(0, 0, 0, 0, (short) 0, 0, (short) 18, 2);
+                //ClientAnchor anchor = new HSSFClientAnchor(0, 0, 0, 0, (short) 0, 0, (short) 18, 2);
+                //ClientAnchor anchor = new HSSFClientAnchor(0, 0, 0, 0, (short) 0, 0, (short) 18, 2);
+                anchor.setAnchorType(ClientAnchor.AnchorType.MOVE_AND_RESIZE); //изменение размера при изменении ячейки
+                //newSheet.createDrawingPatriarch().createPicture(anchor, pictureIndex);
+                sourceSheet.createDrawingPatriarch().createPicture(anchor, pictureIndex);
 
 //        //сохранение в файл
 //        FileOutputStream out = new FileOutputStream("output.xls");
 //        workbook.write(out);
 //        out.close();
 //        workbook.close();
+
+            }
 
             //сохранение в файл
             FileOutputStream out = new FileOutputStream("output.xls");
